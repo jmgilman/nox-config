aeafae0fba38b6b921aa5033be6f19a9f984e3fc
Joshua Gilman <joshuagilman@gmail.com>
2022-02-18 17:58:44 -0800

    Adds pytest

---
+++ nox_config/testing.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+
+import nox
+
+from nox_config.tool import Ctx, Tool
+
+
+@dataclass
+class Tester(Tool):
+    """A tool which is used for testing source code.
+
+    This class provides a loose interface for tools which fall under the
+    category of testers.
+    """
+
+    def test(self, session: nox.Session, files: list[str], ctx: Ctx = Ctx()):
+        """Tests the given list of files.
+
+        Args:
+            session: The session in which to run this tool.
+            files: A list of files to test.
+        """
+        pass
+
+
+@dataclass
+class Pytest(Tester):
+    """A class for interacting with the pytest testing suite.
+
+    Args:
+        config: An optional configuration file to pass with all executions.
+    """
+
+    binary: str = "pytest"
+    config: str = ""
+
+    def run(self, session: nox.Session, ctx: Ctx = Ctx()):
+        if self.config:
+            ctx += Ctx(flags=["-c", self.config])
+
+        super().run(session, ctx)
+
+    def test(self, session: nox.Session, files: list[str], ctx: Ctx = Ctx()):
+        ctx += Ctx(args=files)
+        self.run(session, ctx)
---
+++ tests/test_testing.py
@@ -0,0 +1,28 @@
+from unittest import mock
+
+from nox_config import testing, tool
+
+
+def test_pytest_run(session: mock.MagicMock):
+    p = testing.Pytest(config="test.conf")
+    ctx = tool.Ctx()
+    p.run(session, ctx)
+
+    session.run.assert_called_once_with("pytest", "-c", "test.conf", env={})
+
+
+def test_pytest_setup(session: mock.MagicMock):
+    p = testing.Pytest(deps=["dep1"])
+    p.setup(session)
+
+    session.install.assert_called_once_with("pytest", "dep1")
+
+
+def test_pytest_lint(session: mock.MagicMock):
+    p = testing.Pytest(config="test.conf")
+    ctx = tool.Ctx()
+    p.test(session, ["file1", "file2"], ctx)
+
+    session.run.assert_called_once_with(
+        "pytest", "-c", "test.conf", "file1", "file2", env={}
+    )
